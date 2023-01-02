1660f196b4584031a24f62aa7feb1843683cbadf
yovelzig <yovel20031@gmail.com>
2023-01-02 19:24:27 +0200

    teshuva

--- txtfind.c
+++ txtfind.c
@@ -36,8 +36,36 @@ int similar (char *s, char *t, int n)
     int len1 = strlen(s);
     int len2 = strlen(t);
     int count = 0, i=0, j=0;
+    if (len2 > len1)
+    {
+        if ((len2 - len1) != n)
+    {//printf("pr b");
+        return 0;
+    }
+    while (i<len1 && j<len2)
+    {
+        if (s[i] == t[j])
+        {
+            i++;
+            j++;
+        }
+        else
+        {
+            j++;
+            count++;
+        }
+        if (count<=n)
+        {
+            //printf("pr c");
+           return 1;
+        }   
+    }
+    return 0; 
+    }
+    else
+    {
     if ((len1 - len2) != n)
-    {printf("pr b");
+    {//printf("pr b");
         return 0;
     }
     while (i<len1 && j<len2)
@@ -54,9 +82,10 @@ int similar (char *s, char *t, int n)
         }
         if (count<=n)
         {
-            printf("pr c");
+            //printf("pr c");
            return 1;
         }   
+    }
     }
     return 0;    
 }
@@ -65,7 +94,7 @@ void print_lines(char *str)
 {
     char line[LINE];
     while (fgets(line, LINE, stdin) != NULL)
-    {printf("no pr");
+    {//printf("no pr");
         if (substring(line, str) == 1)
         {
             char *token = strtok(line, "\r\n");
@@ -115,7 +144,7 @@ void print_similar_words(char *str)
         for (char *word =strtok(line, delimiter); word != NULL; word = strtok(NULL, delimiter))
         {    
             if (similar(word, str, 1) == 1)
-            {printf("no a");
+            {//printf("no a");
                 printf("%s\n", word);
             }
         }
@@ -126,9 +155,9 @@ int main()
 {
     char line[LINE];
     fgets(line, LINE , stdin);
-    char *tok = strtok(line, " ");   // token is the first string
-    char *tok2 = strtok(NULL, " "); // token is the second string - the char
-    char ch = tok2[0];              // finding the char at the end of the string
+    char *tok = strtok(line, " ");   
+    char *tok2 = strtok(NULL, " ");
+    char ch = tok2[0];              
     if (ch == 'a')
     {
         print_lines(tok);
